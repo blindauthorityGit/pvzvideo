018895da9aee141d6e5f1ed59844a9c54fa93ef4
Johannes <office@atelierbuchner.at>
2024-12-16 14:29:41 +0100

    neues vid

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function NoJS({ activeVideo }) {
         <div style={{ padding: 0 }}>
             {activeVideo ? (
                 <video
-                    src={"https://atelierbuchner.at/pvz/weihnachten.mp4"}
+                    src={"https://atelierbuchner.at/pvz/weihnachten2.mp4"}
                     controls
                     autoPlay
                     loop
